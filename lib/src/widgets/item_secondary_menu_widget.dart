fcf21bbca22a9293a4add506544eb13c176241f1
Angel Garcia <angelgarciaweb@gmail.com>
2022-02-14 10:50:32 -0400

    Ajustes de margen

--- lib/src/widgets/item_secondary_menu_widget.dart
+++ lib/src/widgets/item_secondary_menu_widget.dart
@@ -68,7 +68,7 @@ class _ItemDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 5),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
